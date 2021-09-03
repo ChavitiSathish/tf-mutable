8ad9dc1e992c31ce6f6b9f7f138ad11e98b7c128
Chaviti Sathish <chavitisathish@gmail.com>
2021-09-04 01:26:14 +0530

    terraform-mutable

---
+++ vpc/gateway.tf
@@ -0,0 +1,22 @@
+resource "aws_internet_gateway" "igw" {
+  vpc_id            = aws_vpc.main.id
+
+  tags              = {
+    Name            = "${var.ENV}-igw"
+  }
+}
+
+resource "aws_eip" "ngw" {
+  vpc               = true
+}
+
+resource "aws_nat_gateway" "ngw" {
+  allocation_id     = aws_eip.ngw.id
+  subnet_id         = aws_subnet.public.*.id[0]
+
+  tags              = {
+    Name            = "${var.ENV}-ngw"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
