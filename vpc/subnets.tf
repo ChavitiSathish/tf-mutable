68ac6399d66cf63d9ff1354467b3578f7a92bc74
Chaviti Sathish <chavitisathish@gmail.com>
2021-09-04 01:19:20 +0530

    terraform-mutable

---
+++ vpc/subnets.tf
@@ -0,0 +1,24 @@
+resource "aws_subnet" "private" {
+  count                   = length(var.PRIVATE_SUBNET_CIDRS)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = element(var.PRIVATE_SUBNET_CIDRS, count.index)
+  availability_zone       = element(var.SUBNETS_AZ, count.index)
+
+  tags                    = {
+    Name                  = "${var.ENV}-private-${count.index+1}"
+  }
+}
+
+resource "aws_subnet" "public" {
+  depends_on              = [aws_vpc_ipv4_cidr_block_association.public_cidr]
+  count                   = length(var.PUBLIC_SUBNET_CIDRS)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = element(var.PUBLIC_SUBNET_CIDRS, count.index)
+  availability_zone       = element(var.SUBNETS_AZ, count.index)
+  map_public_ip_on_launch = true
+
+  tags                    = {
+    Name                = "${var.ENV}-public-${count.index+1}"
+  }
+}
+
--- vpc/vars.tf
+++ vpc/vars.tf
@@ -1,5 +1,6 @@
 variable "VPC_PRIVATE_CIDR"         {}
 variable "VPC_PUBLIC_CIDR"          {}
 variable "ENV"                      {}
-
-//
+variable "PRIVATE_SUBNET_CIDRS"     {}
+variable "PUBLIC_SUBNET_CIDRS"      {}
+variable "SUBNETS_AZ"               {}
