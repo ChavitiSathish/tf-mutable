00873df6191f30e3b7c22e8c7b8729e6f18ab142
Chaviti Sathish <chavitisathish@gmail.com>
2021-09-06 20:06:23 +0530

    terraform-mutable

--- vpc/route-tables.tf
+++ vpc/route-tables.tf
@@ -8,7 +8,7 @@ resource "aws_route_table" "public" {
 
   route {
     vpc_peering_connection_id  = aws_vpc_peering_connection.to-default-vpc.id
-    cidr_block            = var.DEFAULT_VPC_CIDR
+    cidr_block                 = var.DEFAULT_VPC_CIDR
   }
 
   tags                    = {
@@ -28,7 +28,7 @@ resource "aws_route_table" "private" {
 
   route {
     vpc_peering_connection_id  = aws_vpc_peering_connection.to-default-vpc.id
-    cidr_block            = var.DEFAULT_VPC_CIDR
+    cidr_block                 = var.DEFAULT_VPC_CIDR
   }
 
   tags                    = {
